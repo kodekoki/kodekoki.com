6cf9bc373b2e6df6233287f14f7b81b0854256ea
dwikiarlan <arrlancore@gmail.com>
2019-10-15 23:19:13 +0700

    first post

---
+++ src/content/blog/introduce-my-blog/index.md
@@ -0,0 +1,29 @@
+---
+title: Holla, Introduce My Blog
+date: 2019-10-15
+author: Dwiki Arlan
+featured: false
+published: true
+image: ./imagebg.jpg
+description: This is why I start to blog
+category:
+  - Greetings
+tags:
+  - Gatsby
+  - React
+  - Blog System
+---
+
+## Holaa
+
+Here is some composition of this blog that I use for:
+
+- Gatsby (Static Generator)
+- React (Front End)
+- GraphQL (Query Language - Make your static data query able)
+- Netlify (Hosting)
+- Emotion (JSS Styling)
+
+But, actually I do some spy 😊 -ing to the repo of two popular blog that use gatsby which is Kent C Dods Blog & Overreacted. Keep silent please :)
+
+I think that is normal for the composition stack but if you still curious go ahead check [this repo](https://github.com/arrlancore/arlan.net), may be you can improve my code.
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,13 +1,32 @@
 import React from 'react'
 import { Link } from 'gatsby'
-import { css } from '@emotion/core'
-// import useInterval from '../components/useInterval'
+import { css, keyframes } from '@emotion/core'
 import Software from '../images/software.png'
 import Background from '../assets/home-background.svg'
 import LinkedIn from '../assets/icon/linkedin.svg'
 import Github from '../assets/icon/github.svg'
 import Twitter from '../assets/icon/twitter.svg'
 import { ThemeProvider } from '../context'
+import SEO from '../components/seo'
+
+const dotAnimation = keyframes`
+  0% {
+    transform: scale(1);
+    background: #3b2750;
+  }
+  20%{
+    transform: scale(0.5);
+    background: #444;
+  }
+  40%{
+    transform: scale(0.8);
+    background: #blue;
+  }
+  80%{
+    transform: scale(0.6);
+    background: #3b2750;
+  }
+`
 
 const rootStyle = theme => css`
   display: flex;
@@ -16,8 +35,25 @@ const rootStyle = theme => css`
   flex-direction: column;
   width: 100%;
   min-height: 100vh;
-  background-color: ${theme.colors.black};
-  transition: all 0.5s ease-in;
+  background-color: #ff9d00;
+  background-image: url("data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' width='100%25' height='100%25' viewBox='0 0 1600 800'%3E%3Cg stroke='%23000' stroke-width='66.7' stroke-opacity='0.05' %3E%3Ccircle fill='%23ff9d00' cx='0' cy='0' r='1800'/%3E%3Ccircle fill='%23fe8b1d' cx='0' cy='0' r='1700'/%3E%3Ccircle fill='%23fa792d' cx='0' cy='0' r='1600'/%3E%3Ccircle fill='%23f46839' cx='0' cy='0' r='1500'/%3E%3Ccircle fill='%23eb5843' cx='0' cy='0' r='1400'/%3E%3Ccircle fill='%23e04a4c' cx='0' cy='0' r='1300'/%3E%3Ccircle fill='%23d23e53' cx='0' cy='0' r='1200'/%3E%3Ccircle fill='%23c33459' cx='0' cy='0' r='1100'/%3E%3Ccircle fill='%23b12c5d' cx='0' cy='0' r='1000'/%3E%3Ccircle fill='%239f2760' cx='0' cy='0' r='900'/%3E%3Ccircle fill='%238b2460' cx='0' cy='0' r='800'/%3E%3Ccircle fill='%2377225f' cx='0' cy='0' r='700'/%3E%3Ccircle fill='%2363215c' cx='0' cy='0' r='600'/%3E%3Ccircle fill='%234e1f57' cx='0' cy='0' r='500'/%3E%3Ccircle fill='%233a1d50' cx='0' cy='0' r='400'/%3E%3Ccircle fill='%23271948' cx='0' cy='0' r='300'/%3E%3Ccircle fill='%2313153e' cx='0' cy='0' r='200'/%3E%3Ccircle fill='%23000f33' cx='0' cy='0' r='100'/%3E%3C/g%3E%3C/svg%3E");
+  background-attachment: fixed;
+  background-size: cover;
+  transition: all 0.7s ease-in;
+  & .dot {
+    width: 14px;
+    height: 14px;
+    background: #3b2750;
+    position: absolute;
+    left: 6px;
+    top: 6px;
+    border-radius: 50%;
+    animation: ${dotAnimation} 8s ease-in-out infinite;
+    transition: all ease-in-out 1s;
+    & :hover {
+      opacity: 0;
+    }
+  }
   & > main {
     background-color: ${theme.colors.background};
     background-image: url(/static/assets/home-background.svg);
@@ -93,19 +129,11 @@ const menuStyle = theme => css`
 `
 
 const HomePage = () => {
-  // let [count, setCount] = React.useState(0)
-  // let [delay, setDelay] = React.useState(1000)
-
-  // useInterval(() => {
-  //   // Your custom logic here
-  //   setCount(count + 1)
-  //   if (count >= 10) {
-  //     setDelay(null)
-  //   }
-  // }, delay)
   return (
     <ThemeProvider>
+      <SEO title={`Dwiki Arlan Personal Web & Blog`} />
       <div css={rootStyle}>
+        <div className="dot"></div>
         <main>
           <div>
             <Background
@@ -135,6 +163,7 @@ const HomePage = () => {
                 </h2>
               </div>
               <div
+                id="software"
                 css={theme => css`
                   background-color: ${theme.colors.lightBlue};
                   background-image: url(${Software});
@@ -148,9 +177,10 @@ const HomePage = () => {
                   position: relative;
                   margin: 0 0.5rem;
                   background-position-y: 50%;
-                  transition: all ease-in 0.5s;
+                  transition: all ease-in 0.6s;
                   &:hover {
                     background-color: #fff;
+                    transform: scale(1.1);
                   }
                 `}
               ></div>
