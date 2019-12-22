6d6a7ec95ae150411e213324b07ad8f8097ca325
dwikiarlan <arrlancore@gmail.com>
2019-12-23 05:52:26 +0700

    add css

--- src/constants/pageTitle.js
+++ src/constants/pageTitle.js
@@ -1,13 +1,13 @@
 export const SNIPPET = {
   TITLE: 'Kode Snippet',
   DESCRIPTION:
-    'Kumpulan best practice dan tips dari komunitas software developer',
+    'Kumpulan kode reusable terbaik yang dapat digunakan untuk setiap project mu',
 }
 
 export const VIDEO = {
   TITLE: 'Video Latihan',
   DESCRIPTION:
-    'Belajar sambil berlatih dengan video, tools terbaru dan penjelasan yang mudah dipahami',
+    'Belajar sambil berlatih dengan video, tools terbaru dan dengan penjelasan yang ringan',
 }
 
 export const TAGS = {
